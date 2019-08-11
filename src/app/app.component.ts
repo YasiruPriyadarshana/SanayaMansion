8bf1516824ca4ef33d1fe69c32b5a8a7e8339de4
Yasiru Priyadarshana <hly.priyadarshana@gmail.com>
2019-08-11 08:38:01 +0530

    Initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { from } from 'rxjs';
+
+
 @Component({
-  selector: 'app-root',
+    selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SanayaMansion';
+ 
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
