85e2ceda6ec4d7d1596dfe4dcea93e5e43cb128b
Yasiru Priyadarshana <hly.priyadarshana@gmail.com>
2019-08-26 14:49:28 +0530

    nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,15 @@ import { StoresComponent } from './stores/stores.component';
 import { CateringComponent } from './catering/catering.component';
 import { HeaderComponent } from './header/header.component';
 import { DropDownDirective } from './shared/dropdown.directive';
-import { SanayaHomeComponent } from './sanaya-home/sanaya-home.component'
+import { SanayaHomeComponent } from './sanaya-home/sanaya-home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list'
 
 @NgModule({
   declarations: [
@@ -19,12 +27,20 @@ import { SanayaHomeComponent } from './sanaya-home/sanaya-home.component'
     CateringComponent,
     HeaderComponent,
     DropDownDirective,
-    SanayaHomeComponent
+    SanayaHomeComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
