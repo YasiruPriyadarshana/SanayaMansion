108bf10ca39b1efe0e85d73e7ebbdf727adf64ca
Yasiru Priyadarshana <hly.priyadarshana@gmail.com>
2019-08-26 13:19:47 +0530

    home

---
+++ src/app/sanaya-home/sanaya-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sanaya-home',
+  templateUrl: './sanaya-home.component.html',
+  styleUrls: ['./sanaya-home.component.css']
+})
+export class SanayaHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
