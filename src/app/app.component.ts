1b9b6de7695506ca037d420ed401ba06e363256e
Yasiru Priyadarshana <hly.priyadarshana@gmail.com>
2019-08-27 23:50:58 +0530

    side bar full

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 
-
+declare var $:any;
 
 @Component({
     selector: 'app-root',
@@ -9,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
+  ngOnInit(){
+    $('.tree-toggle').click(function () {	$(this).parent().children('ul.tree').toggle(200);
+      });
+    $(function(){
+    $('.tree-toggle').parent().children('ul.tree').toggle(200);
+      })
+  }
 }
