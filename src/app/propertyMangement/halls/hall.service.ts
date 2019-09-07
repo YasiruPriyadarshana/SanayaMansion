b515faf70ed08671391c8d097944edf091a70e74
senthusj <senthu231997@gmail.com>
2019-09-07 15:13:28 +0530

    hall

--- src/app/propertyMangement/halls/hall.service.ts
+++ src/app/propertyMangement/halls/hall.service.ts
@@ -7,9 +7,9 @@ export class HallService {
   hallSelected = new EventEmitter<Hall>();
 
   private  halls : Hall[] = [
-    new Hall('shapire','https://www.venue.royalholloway.ac.uk/wp-content/uploads/2018/02/Founders-Dining-Hall-wedding-1200x630.jpg','100,000','new hall'),
-    new Hall('platinum','https://www.burgfeistritz.com/wp-content/uploads/2018/07/wedding16-1200x630.jpg','100,000','new hall'),
-    new Hall('Vebber','https://www.manila-hotel.com.ph/wp-content/uploads/2018/01/5-1-1200x630.jpg','100,000','new hall'),  
+    new Hall('shapire','https://www.venue.royalholloway.ac.uk/wp-content/uploads/2018/02/Founders-Dining-Hall-wedding-1200x630.jpg','100,000','Advance 25,000rs | capacity-200'),
+    new Hall('platinum','https://www.burgfeistritz.com/wp-content/uploads/2018/07/wedding16-1200x630.jpg','100,000','Advance 35,000rs |  capacity-250'),
+    new Hall('Vebber','https://www.manila-hotel.com.ph/wp-content/uploads/2018/01/5-1-1200x630.jpg','100,000','Advance 40,000rs |  capacity-300'),  
     ];
       getHalls(){
           return this.halls.slice();
